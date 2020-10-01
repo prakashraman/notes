390e2057902e80ab80d80a112ea143a35f440690
Prakash Raman <prakash.raman.ka@gmail.com>
2020-10-01 23:34:07 +0200

    added figlet

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
 import * as inquirer from "inquirer";
+import * as chalk from "chalk";
+import * as figlet from "figlet";
+
 import { setup } from "./setup";
 import { createNote, listNotes } from "./notes";
-import * as chalk from "chalk";
+import { log } from "./helpers";
 
-console.log(chalk.green("Welcome to", chalk.bold("write-lite")), "\n");
+/**
+ * Entry method of the library. It asks all the right questions :)
+ *
+ */
+const start = () => {
+  inquirer
+    .prompt({
+      name: "choice",
+      message: "What would you like to do?",
+      type: "list",
+      choices: [
+        { value: "setup", name: "Setup lite blog" },
+        { value: "new", name: "Create note" },
+        { value: "list", name: "List notes" },
+        { value: "build", name: "Build HTML content" },
+      ],
+    })
+    .then((answer) => {
+      if (answer.choice === "setup") setup();
+      else if (answer.choice === "list") listNotes();
+      else if (answer.choice === "new") createNote();
+    });
+};
 
-inquirer
-  .prompt({
-    name: "choice",
-    message: "What would you like to do?",
-    type: "list",
-    choices: [
-      { value: "setup", name: "Setup lite blog" },
-      { value: "new", name: "Create note" },
-      { value: "list", name: "List notes" },
-      { value: "build", name: "Build HTML content" },
-    ],
-  })
-  .then((answer) => {
-    if (answer.choice === "setup") setup();
-    else if (answer.choice === "list") listNotes();
-    else if (answer.choice === "new") createNote();
-  });
+figlet("Write lite", (er, data) => {
+  if (er) {
+    return log.blue("write-lite");
+  }
+
+  log.log(data);
+  start();
+});
