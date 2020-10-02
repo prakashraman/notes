5bcfe31314d361f39fb23b770d4df888d4a51b21
Prakash Raman <prakash.raman.ka@gmail.com>
2020-10-02 23:39:32 +0200

    build script complete

--- src/build.ts
+++ src/build.ts
@@ -1,8 +1,17 @@
-import { rmdirSync, existsSync, mkdirSync, writeFileSync } from "fs";
+import {
+  rmdirSync,
+  existsSync,
+  mkdirSync,
+  writeFileSync,
+  readFileSync,
+} from "fs";
+import * as showdown from "showdown";
+import * as path from "path";
 
 import { log, getAbsolutePath } from "./helpers";
 import { getIManifest } from "./manifest";
 import { DIST_PATH, DIST_NOTES_PATH, IMANIFEST_PATH } from "./config/constants";
+import { Note } from "./typings";
 
 /**
  * Cleans the dist folder of all remnants
@@ -36,6 +45,35 @@ const writeIManifest = () => {
 };
 
 /**
+ * Converts the note's mardown into HTML and write to a file in
+ * build folder
+ *
+ * @param {Note} note
+ */
+const writeHTMLNote = (note: Note) => {
+  log.blue(`Converting markdown to HTML for ${note.path} ...`);
+
+  const md = readFileSync(getAbsolutePath(note.path), { encoding: "utf-8" });
+  const html = new showdown.Converter().makeHtml(md);
+
+  log.success(`Successfully converted to HTML`);
+
+  const filePath = `${DIST_NOTES_PATH}/${path.parse(note.path).name}.html`;
+
+  log.blue(`Writing HTML file ${filePath} ...`);
+  writeFileSync(getAbsolutePath(filePath), html);
+  log.success(`Successfully create HTML file at ${filePath} ...`);
+};
+
+/**
+ * Iterates over all the notes and converts to HTML
+ *
+ */
+const writeHTMLNotes = () => {
+  getIManifest().notes.forEach((note) => writeHTMLNote(note));
+};
+
+/**
  * Entry point to the build process. Cleans the build folder and recreates it.
  * Ideally should be run on the CD flow and build folder is to be
  * pushed to a publicly accessible server
@@ -49,6 +87,7 @@ const build = () => {
   log.success(`Created build folder at ${DIST_PATH} ...`);
 
   writeIManifest();
+  writeHTMLNotes();
 };
 
 export { build };
